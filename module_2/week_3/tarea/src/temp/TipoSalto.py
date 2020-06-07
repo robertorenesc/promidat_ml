91d2823482bb51ea9501946c8b0c3a56333f4860
Roberto Salazar <robertorenesc@gmail.com>
2020-06-07 16:14:19 -0600

    Revert "Added week 3 resources"
    This reverts commit a742ca90c4b86063c8c9fe7559085bca9583ed3a.

--- module_2/week_3/tarea/src/temp/TipoSalto.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class TipoSalto(Enum):
-
-    SALTO_MINIMO = -1
-    SALTO_PROMEDIO = 0
-    SALTO_MAXIMO = 1
-    
--- module_3/week_1/homework/src/Question_1.py
+++
@@ -1,8 +0,0 @@
-from IndexesCalculator import IndexesCalculator
-from DataFrameUtils import DataFrameUtils
-
-matrix = [[892254, 212],
-          [8993,   300]]
-confusion_matrix = DataFrameUtils.create_dataframe(matrix, indexes = ["NO", "SI"], columns = ["NO", "SI"])
-calculator = IndexesCalculator(confusion_matrix)
-calculator.show_confusion_matrix_indexes()
